88cf0dacc9ef40e49fda4ce00fa5f0f77a1856bf
Almir <almirjunior_bp@hotmail.com>
2024-07-11 12:25:08 -0300

    Adição de mapas interativos

--- boxplot.py
+++ boxplot.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import plotly.express as px
 
-
 st.header('Boxplot das Variáveis Numéricas')
-selected_boxplot_column = st.selectbox('Selecione a coluna para o boxplot', options=st.session_state.df.select_dtypes(include=['number']).columns)
-fig_box = px.box(st.session_state.df, y=selected_boxplot_column, title=f'Boxplot de {selected_boxplot_column}')
-st.plotly_chart(fig_box)
+
+if 'df' in st.session_state:
+    selected_boxplot_column = st.selectbox('Selecione a coluna para o boxplot', options=st.session_state.df.select_dtypes(include=['number']).columns)
+    fig_box = px.box(st.session_state.df, y=selected_boxplot_column, title=f'Boxplot de {selected_boxplot_column}')
+    st.plotly_chart(fig_box)
+else:
+    st.text('Primeiro, escolha um arquivo de dados na tela inicial')
--- dispersao.py
+++ dispersao.py
@@ -2,6 +2,10 @@ import streamlit as st
 import plotly.express as px
 
 st.header('Relação entre Temperatura, Salinidade e Profundidade')
-fig_rel = px.scatter(st.session_state.df, x='Temperatura [°c]', y='Salinidade [psu]', color='Profundidade [m]')
-fig_rel.update_layout(coloraxis_reversescale=True)  # Invertendo a paleta de cores
-st.plotly_chart(fig_rel)
+
+if 'df' in st.session_state:
+    fig_rel = px.scatter(st.session_state.df, x='Temperatura [°c]', y='Salinidade [psu]', color='Profundidade [m]')
+    fig_rel.update_layout(coloraxis_reversescale=True)  # Invertendo a paleta de cores
+    st.plotly_chart(fig_rel)
+else:
+    st.text('Primeiro, escolha um arquivo de dados na tela inicial')
